c68b8262765eb4870a34c52d996bb32869e86504
EmmanuelMaloloy-on <107686611+EmmanuelMaloloy-on@users.noreply.github.com>
2022-06-25 14:38:59 +0800

    Add Login and Ticket classes and views

---
+++ CustomerSupportManager/Controllers/TicketsController.cs
@@ -0,0 +1,19 @@
+﻿using CustomerSupportManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerSupportManager.Controllers
+{
+    public class TicketsController : Controller
+    {
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            List<TicketModel> tickets = new List<TicketModel>();
+            return View(tickets);
+        }
+    }
+}
---
+++ CustomerSupportManager/Models/TicketModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSupportManager.Models
+{
+    public class TicketModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Category { get; set; }
+        public string Product { get; set; }
+    }
+}
